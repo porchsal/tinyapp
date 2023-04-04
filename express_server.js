eb5f625bc683bf0eae3fbbbd9cf834f9177698d8
porchsal <porchsal@yahoo.com>
2023-04-03 23:48:09 -0400

    server with first POST

--- express_server.js
+++ express_server.js
@@ -4,12 +4,17 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 
 };
+
+const generateRandomString = function(){
+    return Math.random().toString(20).slice(2, 8);
+}
 
 app.get("/", (req, res) => {
     res.send("Hello!");
@@ -33,10 +38,19 @@ app.get("/urls.json", (req, res) => {
     res.render("urls_index", templateVars);
   });
 
+  app.get("/urls/new", (req,res) => {
+    res.render("urls_new");
+  })
+
   app.get("/urls/:id", (req, res) => {
     const templateVars = { id: req.params.id, longURL: urlDatabase.id };
     res.render("urls_show", templateVars);
   });
+
+  app.post("/urls", (req,res) => {
+    console.log(req.body);// Log the POST request body to the console
+    res.send(generateRandomString()); // Respond with 'random string created'
+  })
 
 
   module.exports = { urlDatabase };
