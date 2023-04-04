540fc1e46f5725fad34642ee78ce6d50f96a9ebf
porchsal <porchsal@yahoo.com>
2023-04-04 00:41:21 -0400

    server with adding urls

--- express_server.js
+++ express_server.js
@@ -48,9 +48,14 @@ app.get("/urls.json", (req, res) => {
   });
 
   app.post("/urls", (req,res) => {
-    console.log(req.body);// Log the POST request body to the console
-    res.send(generateRandomString()); // Respond with 'random string created'
-  })
+    //  console.log(req.body);// Log the POST request body to the console
+    //  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+    const longURL = req.body.longURL;
+    const shortURL = generateRandomString();
+    urlDatabase[shortURL] = longURL;
+    res.redirect("/urls");  
+
+})
 
 
-  module.exports = { urlDatabase };
+  
