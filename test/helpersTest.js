a41f125d64e4736e2ec3540544f75475984ad4bb
porchsal <porchsal@yahoo.com>
2023-04-10 20:58:19 -0400

    helpersTest.js test updated

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -4,22 +4,24 @@ const { getUserByEmail } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers)
+    const user = getUserByEmail("user@example.com", testUsers).id;
     const expectedUserID = "userRandomID";
     // Write your assert statement here
+    assert.equal(user, expectedUserID);
+
   });
 
   it('should return undefined when looking for a email not in DB', () => {
