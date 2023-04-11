ad2ea343911d559ff20d33c8895aef8259482a04
porchsal <porchsal@yahoo.com>
2023-04-10 20:59:42 -0400

    helpers.js function updated

--- helpers.js
+++ helpers.js
@@ -1,24 +1,34 @@
-const generateRandomString = function(str){
-    return Math.random().toString(20).slice(2, str);
-}
+const generateRandomString = function(str) {
+  return Math.random().toString(20).slice(2, str);
+};
 
 const getUserByEmail = function(email, usersDB) {
-  for (const user in usersDB){
-    if(usersDB[user].email === email) {
+  for (const user in usersDB) {
+    if (usersDB[user].email === email) {
       return usersDB[user];
     }
   }
   return undefined;
-}
+};
+
+const getUserIDByEmail = function(email, usersDB) {
+  for (const user in usersDB) {
+    if (usersDB[user].email === email) {
+      return user;
+    }
+  }
+  return undefined;
+};
+
 
 const urlForUser = function(id, database) {
-    let userUrls = {};
-    //const userID = getUserIDByEmail(id, users);
-    for(const shortURL in database){
-      if(database[shortURL].userID === id){
-        userUrls[shortURL] = database[shortURL];
-      }
+  let userUrls = {};
+  //const userID = getUserIDByEmail(id, users);
+  for (const shortURL in database) {
+    if (database[shortURL].userID === id) {
+      userUrls[shortURL] = database[shortURL];
     }
-    return userUrls;
   }
-module.exports = {generateRandomString, getUserByEmail, urlForUser};
+  return userUrls;
+};
+module.exports = {generateRandomString, getUserByEmail, urlForUser, getUserIDByEmail};
